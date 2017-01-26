e0634c90bfccec9b5be01a2f93ff741503c3019b
Timothy Jones <tim@zmthy.net>
2017-01-27 09:53:40 +1300

    Correct accept function, specialise function types
    The change to the accept function caused it to run the given action
    regardless of whether or not the relevant header matched. This change
    replaces the action with 'pass'.
    
    Generalising the two functions to work on any instance of FromHeader
    meant that it was much more difficult to work with OverloadedStrings,
    since both MediaType and Language are instances of IsString. Now
    functions that specialise the input type to those two types are also
    provided, and the two types are exported so the user does not need to
    require http-media to explicitly specify the types.

--- src/Snap/Accept.hs
+++ src/Snap/Accept.hs
@@ -1,16 +1,27 @@
 ------------------------------------------------------------------------------
+-- | HTTP media type functionality as a complement to Snap's 'method' and
+-- 'methods' functions.  Branches based on the value of the Accept or
+-- Accept-Language header of the current request, automatically setting the
+-- Content-Type or Content-Language header of the response as appropriate.
 module Snap.Accept
     (
     -- * Branching
       accept
+    , acceptMedia
+    , acceptLanguage
     , accepts
+    , acceptsMedia
+    , acceptsLanguage
+
+    -- * Accept types
+    , MediaType
+    , Language
 
     -- * Header names
     , FromHeader (..)
     ) where
 
-------------------------------------------------------------------------------
-import           Control.Monad                   (join, (>=>))
+import           Control.Monad                   (join)
 import           Data.ByteString                 (ByteString)
 import           Data.CaseInsensitive            (CI)
 import           Data.Maybe                      (fromMaybe)
@@ -29,45 +40,61 @@ class (Accept a, RenderHeader a) => FromHeader a where
   requestName :: Proxy a -> CI ByteString
   -- | The name of the corresponding Content-* header for this type.
   responseName :: Proxy a -> CI ByteString
+  -- | The default header value to use if the header is absent.
+  defaultValue :: Proxy a -> ByteString
 
 instance FromHeader MediaType where
   requestName _ = "Accept"
   responseName _ = "Content-Type"
+  defaultValue _ = "*/*"
 
 instance FromHeader Language where
   requestName _ = "Accept-Language"
   responseName _ = "Content-Language"
+  defaultValue _ = "*"
 
 
 ------------------------------------------------------------------------------
 -- | Runs a Snap monad only if the request's Accept* header allows for the
--- given media type. If accepted, the response's Content-* header is
--- automatically filled in.
+-- given value.  If accepted, the response's Content-* header is automatically
+-- filled in.
 accept :: (FromHeader a, MonadSnap m) => a -> m b -> m b
-accept acc action =
-    withAccept (proxyFor acc) (matchAccept [acc]) >>= maybe (run acc) run
+accept a s =
+    withAccept (proxyFor a) (matchAccept [a]) >>= maybe pass (`withHeader` s)
   where
-    run = flip runWithType action
     proxyFor :: a -> Proxy a
     proxyFor _ = Proxy
 
+-- | The 'accept' function but specialised to 'MediaType'.
+acceptMedia :: (MonadSnap m) => MediaType -> m a -> m a
+acceptMedia = accept
+
+-- | The 'accept' function but specialised to 'Language'.
+acceptLanguage :: (MonadSnap m) => Language -> m a -> m a
+acceptLanguage = accept
+
 
 ------------------------------------------------------------------------------
--- | Matches the Accept* header of the request to each of the media types in
--- the pairs of the given list, running the corresponding Snap monad in the
--- pair that is most desired by the client. If two or more results arise with
--- the same quality level and specificity, then the pair that appears earliest
--- in the list is matched. On any match, the response's Content-* header is
+-- | Matches the Accept* header of the request to each of the values in the
+-- pairs of the given list, running the corresponding Snap monad in the pair
+-- that is most desired by the client.  If two or more results arise with the
+-- same quality level and specificity, then the pair that appears earliest in
+-- the list is matched.  On any match, the response's Content-* header is
 -- automatically filled in.
 accepts :: (FromHeader a, MonadSnap m) => [(a, m b)] -> m b
-accepts []   = pass
-accepts dict =
-    withAccept (proxyFor dict) (mapAccept dict') >>=
-    fromMaybe (snd $ head dict')
+accepts d = withAccept (proxyFor d) (mapAccept d') >>= fromMaybe pass
   where
-    dict' = map (join $ fmap . runWithType . fst) dict
+    d' = map (join $ fmap . withHeader . fst) d
     proxyFor :: [(a, m b)] -> Proxy a
     proxyFor _ = Proxy
+
+-- | The 'accepts' function but specialised to 'MediaType'.
+acceptsMedia :: (MonadSnap m) => [(MediaType, m a)] -> m a
+acceptsMedia = accepts
+
+-- | The 'accepts' function but specialised to 'Language'.
+acceptsLanguage :: (MonadSnap m) => [(Language, m a)] -> m a
+acceptsLanguage = accepts
 
 
 ------------------------------------------------------------------------------
@@ -76,16 +103,17 @@ accepts dict =
 withAccept :: (FromHeader a, MonadSnap m)
            => Proxy a
            -> (ByteString -> Maybe b) -> m (Maybe b)
-withAccept proxy f = getsRequest $ getHeader (requestName proxy) >=> f
+withAccept p f = getsRequest $ f . fromMaybe (defaultValue p) . getHeader name
+  where
+    name = requestName p
 
 
 ------------------------------------------------------------------------------
--- | Runs the given Snap monad with the given Accept value's rendered form set
--- in the appropriate header of the response.
-runWithType :: (FromHeader a, MonadSnap m) => a -> m b -> m b
-runWithType acc action = do
-    modifyResponse (setHeader (responseName (proxyFor acc)) $ renderHeader acc)
-    action
+-- | Runs a Snap monad with the rendered value of an Content-* form set in the
+-- appropriate header of the response.
+withHeader :: (FromHeader a, MonadSnap m) => a -> m b -> m b
+withHeader a m = modifyResponse (setHeader name (renderHeader a)) >> m
   where
+    name = responseName (proxyFor a)
     proxyFor :: a -> Proxy a
     proxyFor _ = Proxy
